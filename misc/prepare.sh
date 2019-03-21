cda0362fa3c47d8667c5e42e232455f7b17fd255
YaserEmbedded <eng.yassermahmood@gmail.com>
2019-03-21 15:40:53 +0100

    1- Edited the ops.py to fit the tesla K40c gpu. 2- Edited the prepare.sh to not download the data as I already have it

--- misc/prepare.sh
+++ misc/prepare.sh
@@ -6,10 +6,10 @@ wget https://filebox.ece.vt.edu/~ylzou/eccv2018dfnet/pretrained.tar
 tar -xvf pretrained.tar
 
 # Download training data
-echo "Downloading training data"
-mkdir dataset
-cd dataset
-wget https://filebox.ece.vt.edu/~ylzou/eccv2018dfnet/kitti_5frame_1152_320.tar
-tar -xvf kitti_5frame_1152_320.tar
-cd ..
+#echo "Downloading training data"
+#mkdir dataset
+#cd dataset
+#wget https://filebox.ece.vt.edu/~ylzou/eccv2018dfnet/kitti_5frame_1152_320.tar
+#tar -xvf kitti_5frame_1152_320.tar
+#cd ..
 
